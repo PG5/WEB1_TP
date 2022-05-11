3393f3d228324514475f26906eae750ef914ad21
Paz Guillamon <mguillamon@alumnos.exa.unicen.edu.ar>
2022-05-11 13:22:18 -0300

    agrego saludo en contacto y realizo DOM en practica

--- practica/js/index.js
+++ practica/js/index.js
@@ -1,3 +1,10 @@
-alert ("Hola Mundo!");
-let nombre = prompt("Cual es tu nombre?");
-console.log("Hola " + nombre);
+
+
+let nodo = document.querySelector("#saludo");
+
+function saludoPersonalizado(){
+    let nombre = prompt("Hola, cual es tu nombre");
+    console.log("Hola " + nombre);
+    nodo.innerHTML = "Hola " + nombre;
+};
+
